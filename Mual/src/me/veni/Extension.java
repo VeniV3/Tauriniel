639df2245571a2cd7a5880e4ef11314ed8ece320
VeniV3 <44909823+VeniV3@users.noreply.github.com>
2018-11-09 22:52:40 +0100

    .

---
+++ Mual/src/me/veni/Extension.java
@@ -0,0 +1,33 @@
+package me.veni;
+
+import java.util.Scanner;
+
+public class Extension {
+	
+	Scanner s;
+	
+	public Extension() {
+		s = new Scanner(System.in);
+		roar();
+		123
+	}
+	
+	
+	
+	private void roar() {
+		String odpowiedz = s.nextLine();
+		if(odpowiedz.equals("chuj")) {
+			System.out.println("good");
+			
+		}else if(odpowiedz.equals("dwachuje")){
+			System.out.println("least good");
+	
+			
+			
+		}else {
+			System.out.println("zle");
+		}
+		
+	}
+	//konkretny ci¹g znaków
+}
---
+++ Mual/src/me/veni/Launcher.java
@@ -0,0 +1,9 @@
+package me.veni;
+
+public class Launcher {
+	public static void main(String[] args) {
+		new test();
+		//TYP NAZWA = NOWY tYP();
+	}
+}
+//kliknij 1 wyœwietl 3 wartoœæ
